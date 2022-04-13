62199fe792063bcda38dee5849c27496e8699a3a
Or <or.healthy@healthy.io>
2022-04-13 12:48:43 +0300

    app design still no Control System

--- Concentration_Demo/ViewController.swift
+++ Concentration_Demo/ViewController.swift
@@ -9,8 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var flipCount = 0 {
+        didSet{
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
+    }
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    var emojiChoices = ["👻", "🧠", "👽", "💩", "👻", "🧠", "👽", "💩"]
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        if let cardNumber = cardButtons.firstIndex(of: sender){
+            flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+        }else{
+            print("chosen card is not in card buttons")
+        }
+        
+    }
     
     
-
+    func flipCard(withEmoji emoji: String, on button: UIButton){
+//        print("flipCard(withEmoji: \(emoji))")
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        }else{
+            button.setTitle(emoji, for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+            flipCount += 1
+            }
+        }
 }
+
 
