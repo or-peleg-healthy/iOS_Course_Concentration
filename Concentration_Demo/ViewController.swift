7a6b9ccce9070c6a1c08e6d39da0fcdc8874a6e0
Or <or.healthy@healthy.io>
2022-04-13 09:52:18 +0300

    Initial Commit

---
+++ Concentration_Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Concentration_Demo
+//
+//  Created by Or Peleg on 13/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
